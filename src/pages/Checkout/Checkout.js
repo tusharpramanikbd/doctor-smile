d07637d80a57383549a5805f89edad0b04e0d2f6
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-17 11:24:48 +0600

    created all the page template

---
+++ src/pages/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Checkout.css'
+
+const Checkout = () => {
+  return (
+    <div>
+      <h1>This is checkout page</h1>
+    </div>
+  )
+}
+
+export default Checkout
---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Signup.css'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>This is signup page</h1>
+    </div>
+  )
+}
+
+export default Signup
