2f1d0bd644597d329b80f4e3a91a7f9fa31cf053
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-17 14:02:16 +0600

    login component created

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Form } from 'react-bootstrap'
 import SocialLogin from '../SocialLogin/SocialLogin'
 import './Login.css'
 
@@ -6,6 +7,22 @@ const Login = () => {
   return (
     <div className='container login-container'>
       <SocialLogin />
+
+      <div className='login-form'>
+        <Form>
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type='email' placeholder='Enter email' required />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='formBasicPassword'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' placeholder='Password' required />
+          </Form.Group>
+          <Button variant='primary' type='submit'>
+            Login
+          </Button>
+        </Form>
+      </div>
     </div>
   )
 }
