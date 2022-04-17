2f9ef7592ffd4f75360a243481d85db689e68557
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-17 22:14:06 +0600

    banner section added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <header>
+      <div className='banner-container'>
+        <h1>It's time to smile in style</h1>
+        <h2>A dentist experience that finally puts you at the center</h2>
+        <a href='#services'>BOOK NOW</a>
+        <p>or call us to book at 8801746941437</p>
+      </div>
+    </header>
+  )
+}
+
+export default Banner
