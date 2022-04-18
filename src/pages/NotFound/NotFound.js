d78825cf66d84daf437877f3ea108bc16bf890a4
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-18 09:52:23 +0600

    modified 404 not found page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './NotFound.css'
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='container not-found-container'>
       <h1>This is 404 not found page</h1>
     </div>
   )
